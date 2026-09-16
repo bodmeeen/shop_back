8ad6833b8803b5dec574d7a8ef1dfeb27975c816
bodmeeen <maks.demianp@gmail.com>
2026-09-16 17:03:06 +0300

    Налаштовано Axum сервер та підключено Sqlite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router, extract::State};
+use sqlx::sqlite::SqlitePoolOptions;
+use sqlx::SqlitePool;
+
+mod db;
+
+#[derive(Clone)]
+struct AppState {
+    db: SqlitePool,
 }
+
+#[tokio::main] // Макрос
+async fn main() {
+    dotenvy::dotenv().ok();
+    let db_url = std::env::var("DATABASE_URL").expect("Не знайдено DATABASE_URL");
+
+    let pool = SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect(&db_url)
+        .await
+        .expect("Не вдалося підключитись до бд");
+
+    db::seeding::setup_database(&pool).await;
+
+    let state = AppState { db: pool };
+
+    let app = Router::new()
+        .route("/", get(hello_shop))
+        .with_state(state);
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    println!("Сервер запущений за адресою: http://localhost:3000");
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+// Хендлер (обробник запиту). Звичана ф-я яка повертає текст
+async fn hello_shop() -> &'static str {
+    "Бд підключена"
+}
+
+//     // Роутер; якщо користувач зайшов на головну сторінку, то для
+//     // нього виконується ф-я hello_shop
+//     let app = Router::new()
+//         .route("/", get(hello_shop));
+
+//     // Підключення до порту 3000 локально
+//     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+
+//     println!("Сервер запущений за адресою: http://localhost:3000");
+
+//     // serve це безкінечний цикл, сервер працює поки не перервати 
+//     // його роботу через термінал
+//     axum::serve(listener, app).await.unwrap();
+// }
+
---
+++ src/models/product.rs
@@ -0,0 +1,9 @@
+pub struct Product {
+    pub id: i64,
+    pub title: String,
+    pub body: String,
+    pub old_price: i64,
+    pub price: i64,
+    pub status: String,
+    // pub created_at  // дізнатись який тут тип даних краще підійде
+}
